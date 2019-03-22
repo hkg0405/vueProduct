65cd2457239ee555fbbddc71be6998d856cc1754
hhh <599098339@qq.com>
2019-03-22 10:02:46 +0800

    123213123

---
+++ 1-node简介.js
@@ -0,0 +1,17 @@
+/*
+    Node.js 不是一门新的编程语言,  是一个JavaScript运行环境, 在node的环境下, 我们可以使JavaScript脱离浏览器端独立运行.
+
+    node里有模块化的概念, node里用的npm(包管理器, node package manager)可以快速的进行模块的管理
+
+    我们可以使用node的http模块, 进行服务器的搭建; 或者使用express模块, 进行服务器的快速搭建
+
+    node的特点:
+    1. 单线程
+    2. 异步的非阻塞式I/O(耗时的操作)
+    3. 以事件驱动
+
+    node的适用场景:
+    1. 后台开发
+    2. 使用node自带的npm功能, 方便的安装, 删除, 管理第三方模块   eg: 不是说想用npm就一定要写node代码, npm只要安装了node环境, 可以只用npm, 不写node.js的任何代码
+    3. node的兼容性很好, windows, Linux, MacOS均可使用node环境, node的0.6版本之后, 会直接预装npm功能, 不用单独安装npm了.
+ */
